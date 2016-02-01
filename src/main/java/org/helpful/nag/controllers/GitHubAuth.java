57a44cc27072d1c340db649aeda50b47d8629bd6
chris damour <chris.damour@bodybuilding.com>
2016-02-01 10:24:06 -0700

    linked some articles to help implement the auth magic of GH

---
+++ src/main/java/org/helpful/nag/controllers/GitHubAuth.java
@@ -0,0 +1,16 @@
+package org.helpful.nag.controllers;
+
+public class GitHubAuth {
+
+    //TODO: https://dzone.com/articles/how-to-implement-java-oauth-20-to-sign-in-with-git-1
+    // http://blog.takipi.com/tutorial-how-to-implement-java-oauth-2-0-to-sign-in-with-github-and-google/
+    // https://github.com/google/google-oauth-java-client
+    // http://crunchify.com/how-to-access-github-content-with-basic-oauth-authentication-in-java-httpclient-or-urlconnection-method/
+    // https://help.github.com/articles/git-automation-with-oauth-tokens/
+    // http://requests-oauthlib.readthedocs.org/en/latest/examples/github.html
+    // https://developer.github.com/guides/basics-of-authentication/
+    // https://developer.github.com/v3/oauth/
+    // https://spring.io/guides/tutorials/spring-boot-oauth2/
+    // 
+
+}
